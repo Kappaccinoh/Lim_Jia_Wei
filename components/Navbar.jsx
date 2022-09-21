428449b802e99a2bbe67d649a2617cf12481a51c
Kappaccinoh <lim.jia28@gmail.com>
2022-09-21 17:06:09 +0800

    installed icons using npm

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100]'>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+                <div>
+                    <ul className='hidden md:flex'>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Experience</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                        </Link>
+                    </ul>
+                    <div className=''>
+                        <AiOutlineMenu size={30}/>
+                    </div>
+                </div>
             </div>
         </div>
     )
